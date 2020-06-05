81745deaf5c525144c6d45a7972d04e5eef81867
Ryoji Kurosawa <kuro009900@gmail.com>
2020-06-05 18:20:38 +0900

    fix clang-tidy warnings

--- src/jogasaki/utils/watch.h
+++ src/jogasaki/utils/watch.h
@@ -142,17 +142,17 @@ public:
     }
 
     std::unique_ptr<std::vector<std::size_t>> durations(point_in_code begin, point_in_code end) {
-      auto results = std::make_unique<std::vector<std::size_t>>();
-      Clock::time_point fixed_begin = view_first(begin);
-      Clock::time_point fixed_end = view_last(end);
-      for(auto& p : records_) {
-        auto& arr = p.second;
-        if (arr[begin] == Clock::time_point() && arr[end] == Clock::time_point()) continue; //NOLINT
-        auto e = arr[end] == Clock::time_point() ? fixed_end : arr[end]; //NOLINT
-        auto b = arr[begin] == Clock::time_point() ? fixed_begin : arr[begin]; //NOLINT
-        results->emplace_back(std::chrono::duration_cast<Duration>(e-b).count());
-      }
-      return std::move(results);
+        auto results = std::make_unique<std::vector<std::size_t>>();
+        Clock::time_point fixed_begin = view_first(begin);
+        Clock::time_point fixed_end = view_last(end);
+        for(auto& p : records_) {
+            auto& arr = p.second;
+            if (arr[begin] == Clock::time_point() && arr[end] == Clock::time_point()) continue; //NOLINT
+            auto e = arr[end] == Clock::time_point() ? fixed_end : arr[end]; //NOLINT
+            auto b = arr[begin] == Clock::time_point() ? fixed_begin : arr[begin]; //NOLINT
+            results->emplace_back(std::chrono::duration_cast<Duration>(e-b).count());
+        }
+        return results;
     }
 
 private:

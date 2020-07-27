25d98e83c7efe016d48c78356244bab98c6dbc00
Ryoji Kurosawa <kuro009900@gmail.com>
2020-07-28 05:43:10 +0900

    add ordered variable set

---
+++ src/jogasaki/plan/ordered_variable_set.h
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2018-2020 tsurugi project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <unordered_map>
+
+#include <takatori/descriptor/variable.h>
+
+namespace jogasaki::plan {
+
+/**
+ * @brief ordered variables set used to add ordered unique index to all variables in a request
+ */
+class ordered_variable_set {
+public:
+    using variable = takatori::descriptor::variable;
+    using entity_type = std::vector<variable>;
+    using indices_type = std::unordered_map<variable, std::size_t>;
+
+    static constexpr std::size_t npos = static_cast<std::size_t>(-1);
+    /**
+     * @brief create new empty instance
+     */
+    ordered_variable_set() = default;
+
+    /**
+     * @brief create new instance from map
+     */
+    explicit ordered_variable_set(entity_type entity) :
+        entity_(std::move(entity))
+    {
+        for(std::size_t i=0, n=entity_.size(); i < n; ++i) {
+            indices_[entity_[i]] = i;
+        }
+    }
+
+    std::size_t add(variable v) {  //NOLINT(performance-unnecessary-value-param)
+        if(indices_.count(v) == 0) {
+            entity_.emplace_back(v);
+            indices_[v] = entity_.size()-1;
+        }
+        return indices_[v];
+    }
+
+    /**
+     * @brief remove variable entry
+     * @param v the variable to remove from this object
+     * @return true if requested variable is found and removed successfully
+     * @return false if requested variable is not found
+     * @attention this is not very efficient in its space usage and not intended to be called frequently
+     */
+    bool remove(variable v) {  //NOLINT(performance-unnecessary-value-param)
+        if(indices_.count(v) == 0) {
+            return false;
+        }
+        entity_.emplace_back(v);
+        indices_.erase(v);
+        return true;
+    }
+
+    /**
+     * @brief index accessor for the variable
+     * @param v the variable to find index
+     * @return unique index of the variable, which can be used to totally order the variables in the request
+     * @return npos if the variable is not found
+     */
+    [[nodiscard]] std::size_t index(variable v) const noexcept {  //NOLINT(performance-unnecessary-value-param)
+        if(indices_.count(v) == 0) {
+            return npos;
+        }
+        return indices_.at(v);
+    }
+
+    /**
+     * @brief size accessor
+     * @return the number of variables registered to this object
+     */
+    [[nodiscard]] std::size_t size() const noexcept {
+        return indices_.size();
+    }
+
+private:
+    entity_type entity_{};
+    indices_type indices_{};
+};
+
+}
+
+
---
+++ test/jogasaki/executor/process/ordered_variable_set_test.cpp
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2018-2020 tsurugi project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <jogasaki/plan/ordered_variable_set.h>
+
+#include <string>
+
+#include <yugawara/binding/factory.h>
+#include <yugawara/storage/configurable_provider.h>
+#include <takatori/util/object_creator.h>
+#include <takatori/plan/forward.h>
+#include <gtest/gtest.h>
+
+#include <jogasaki/test_root.h>
+#include <jogasaki/executor/exchange/forward/step.h>
+
+#include <jogasaki/executor/process/mock/task_context.h>
+#include <jogasaki/executor/process/mock/process_executor.h>
+#include <jogasaki/test_utils.h>
+
+namespace jogasaki::plan {
+
+using namespace executor;
+using namespace accessor;
+using namespace takatori::util;
+using namespace std::string_view_literals;
+using namespace std::string_literals;
+
+using namespace jogasaki::memory;
+using namespace boost::container::pmr;
+using namespace yugawara::binding;
+using namespace yugawara::storage;
+
+namespace t = ::takatori::type;
+namespace v = ::takatori::value;
+namespace descriptor = ::takatori::descriptor;
+namespace scalar = ::takatori::scalar;
+namespace relation = ::takatori::relation;
+namespace plan = ::takatori::plan;
+namespace binding = ::yugawara::binding;
+
+using ::takatori::util::downcast;
+using ::takatori::util::string_builder;
+
+using varref = scalar::variable_reference;
+
+using ::takatori::util::fail;
+
+class ordered_variable_set_test : public test_root {
+
+};
+
+TEST_F(ordered_variable_set_test, basic) {
+    factory f;
+
+    ordered_variable_set variables{};
+    auto&& c0 = f.stream_variable("c0");
+    auto&& c1 = f.stream_variable("c1");
+    auto&& c2 = f.stream_variable("c2");
+    auto&& e0c0 = f.exchange_column("e0c0");
+    auto&& e0c1 = f.exchange_column("e0c1");
+    auto&& e0c2 = f.exchange_column("e0c2");
+
+    auto provider = std::make_shared<configurable_provider>();
+
+    std::shared_ptr<::yugawara::storage::table> t0 = provider->add_table("T0", {
+        "T0",
+        {
+            { "C0", t::int8() },
+            { "C1", t::float8 () },
+        },
+    });
+    auto&& t0c0 = f.table_column(t0->columns()[0]);
+    auto&& t0c1 = f.table_column(t0->columns()[1]);
+    variables.add(c0);
+    variables.add(c1);
+    variables.add(c2);
+    variables.add(c1);
+    ASSERT_EQ(3, variables.size());
+    EXPECT_EQ(0, variables.index(c0));
+    EXPECT_EQ(1, variables.index(c1));
+    EXPECT_EQ(2, variables.index(c2));
+    variables.add(e0c0);
+    variables.add(e0c1);
+    variables.add(e0c2);
+    variables.add(e0c2);
+    variables.add(t0c0);
+    variables.add(t0c1);
+    variables.add(t0c1);
+    variables.add(t0c1);
+    ASSERT_EQ(8, variables.size());
+    EXPECT_EQ(3, variables.index(e0c0));
+    EXPECT_EQ(4, variables.index(e0c1));
+    EXPECT_EQ(5, variables.index(e0c2));
+    EXPECT_EQ(6, variables.index(t0c0));
+    EXPECT_EQ(7, variables.index(t0c1));
+    EXPECT_TRUE(variables.remove(t0c1));
+    EXPECT_FALSE(variables.remove(t0c1));
+    ASSERT_EQ(7, variables.size());
+}
+
+}
+

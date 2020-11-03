19db191c9f1867537b01bfd0bf8aa6315b6cf6f1
Ryoji Kurosawa <kuro009900@gmail.com>
2020-11-03 10:09:17 +0900

    add encode()/decode()

---
+++ src/jogasaki/kvs/coder.h
@@ -0,0 +1,242 @@
+/*
+ * Copyright 2018-2020 tsurugi project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <memory>
+#include <cmath>
+#include <boost/endian/conversion.hpp>
+#include <glog/logging.h>
+
+#include <takatori/util/fail.h>
+#include <jogasaki/meta/field_type.h>
+#include <jogasaki/constants.h>
+#include <jogasaki/accessor/record_ref.h>
+
+namespace jogasaki::kvs {
+
+using takatori::util::fail;
+
+template<typename To, typename From>
+static inline To type_change(From from) {
+    static_assert(sizeof(To) == sizeof(From));
+    To t{};
+    std::memcpy(&t, &from, sizeof(From)); // We can expect compiler to optimize to omit this copy.
+    return t;
+}
+
+template<std::size_t N> struct int_n {};
+template<> struct int_n<16> { using type = std::int16_t; };
+template<> struct int_n<32> { using type = std::int32_t; };
+template<> struct int_n<64> { using type = std::int64_t; };
+
+template<std::size_t N> struct float_n {};
+template<> struct float_n<32> { using type = float; };
+template<> struct float_n<64> { using type = double; };
+
+template<std::size_t N> using int_t = typename int_n<N>::type;
+template<std::size_t N> using uint_t = std::make_unsigned_t<int_t<N>>;
+template<std::size_t N> using float_t = typename float_n<N>::type;
+
+template<std::size_t N>
+static constexpr uint_t<N> SIGN_BIT = static_cast<uint_t<N>>(1) << (N - 1); // NOLINT
+
+template<std::size_t N>
+static inline uint_t<N> key_encode(int_t<N> data, bool ascending) {
+    auto u = type_change<uint_t<N>>(data);
+    u ^= SIGN_BIT<N>;
+    if (!ascending) {
+        u = ~u;
+    }
+    return boost::endian::native_to_big(u);
+}
+
+template<std::size_t N>
+static inline void key_decode(int_t<N>& value, uint_t<N> data, bool ascending) {
+    auto u = boost::endian::big_to_native(data);
+    if (!ascending) {
+        u = ~u;
+    }
+    u ^= SIGN_BIT<N>;
+    value = type_change<int_t<N>>(u);
+}
+
+// encode and decode logic for double with considering the key is ascending or not
+template<std::size_t N>
+static inline uint_t<N> key_encode(float_t<N> data, bool ascending) {
+    auto u = type_change<uint_t<N>>(std::isnan(data) ? std::numeric_limits<float_t<N>>::quiet_NaN() : data);
+    if ((u & SIGN_BIT<N>) == 0) {
+        u ^= SIGN_BIT<N>;
+    } else {
+        u = ~u;
+    }
+    if (!ascending) {
+        u = ~u;
+    }
+    return boost::endian::native_to_big(u);
+}
+
+template<std::size_t N>
+static inline void key_decode(float_t<N>& value, uint_t<N> data, bool ascending) {
+    auto u = boost::endian::big_to_native(data);
+    if (!ascending) {
+        u = ~u;
+    }
+    if ((u & SIGN_BIT<N>) != 0) {
+        u ^= SIGN_BIT<N>;
+    } else {
+        u = ~u;
+    }
+    value = type_change<float_t<N>>(u);
+}
+
+//template<class T, std::size_t N = sizeof(T) * bits_per_byte>
+//static inline void write_primitive(Stream &stream, std::any const& value, bool ascending) {
+//    static_assert(std::is_fundamental_v<T>);
+//    if (value.has_value()) {
+//        T val {};
+//        val = utils::any_cast<T>(value);
+//        do_write<uint_t<N>>(stream, key_encode<N>(val, ascending));
+//    } else {
+//        stream.put('\0', sizeof(T), ascending);
+//    }
+//}
+
+class stream {
+public:
+    explicit stream(std::string& s) : base_(s.data()), pos_(0), length_(s.length()) {}
+
+    template<std::size_t N>
+    void do_write(uint_t<N> data) {
+        std::memcpy(base_+pos_, reinterpret_cast<char*>(&data), N/bits_per_byte);
+        pos_ += N/bits_per_byte;
+    }
+
+    template<class T, std::size_t N = sizeof(T) * bits_per_byte>
+    std::enable_if_t<(std::is_integral_v<T> || std::is_floating_point_v<T>) && (N == 32 || N == 64), void> write(T data, bool ascending = true) {
+        do_write<N>(key_encode<N>(data, ascending));
+    }
+
+    void do_write(char const* dt, std::size_t sz, bool ascending) {
+        assert(pos_ + sz <= length_);  // NOLINT
+        if (sz > 0) {
+            if (ascending) {
+                std::memcpy(base_ + pos_, dt, sz);  // NOLINT
+            } else {
+                for (std::size_t i = 0; i < sz; ++i) {
+                    *(base_ + pos_ + i) = ~(*(dt + i));  // NOLINT
+                }
+            }
+        }
+        pos_ += sz;
+    }
+
+    template<class T>
+    std::enable_if_t<std::is_same_v<T, accessor::text>, void> write(T data, bool ascending = true) {
+        std::string_view sv{data};
+        // for key encoding, we are assuming the text is not so long
+        assert(sv.length() < 32768); //NOLINT
+        std::int16_t len{static_cast<std::int16_t>(sv.length())};
+        do_write<16>(key_encode<16>(len, ascending));
+        do_write(sv.data(), sv.size(), ascending);
+    }
+
+//    template<class T>
+//    T read(memory::paged_memory_resource* resource = nullptr) {
+//        T data;
+//        std::memcpy(&data, base_+pos_, sizeof(T));
+//        pos_ += sizeof(T);
+//        return data;
+//    }
+
+    template<std::size_t N>
+    uint_t<N> do_read() {
+        uint_t<N> ret{};
+        std::memcpy(&ret, base_+pos_, N/bits_per_byte);
+        pos_ += N/bits_per_byte;
+        return ret;
+    }
+
+    template<class T, std::size_t N = sizeof(T) * bits_per_byte>
+    std::enable_if_t<(std::is_integral_v<T> || std::is_floating_point_v<T>) && (N == 16 || N == 32 || N == 64), T> read(bool ascending = true) {
+        T value{};
+        key_decode<N>(value, do_read<N>(), ascending);
+        return value;
+    }
+
+    template<class T>
+    std::enable_if_t<std::is_same_v<T, accessor::text>, T> read(memory::paged_memory_resource* resource = nullptr, bool ascending = true) {
+        auto len = read<std::int16_t>(ascending);
+        assert(len >= 0);
+        auto p = static_cast<char*>(resource->allocate(len));
+        if (len > 0) {
+            if (ascending) {
+                std::memcpy(p, base_ + pos_, len);  // NOLINT
+            } else {
+                for (std::size_t i = 0; i < len; ++i) {
+                    *(p + i) = ~(*(base_ + pos_ + i));  // NOLINT
+                }
+            }
+        }
+        accessor::text t{resource, p, static_cast<std::size_t>(len)};
+        return t;
+    }
+
+    void reset() {
+        pos_ = 0;
+    }
+private:
+    char* base_{};
+    std::size_t pos_{};
+    std::size_t length_{};
+};
+
+template<std::size_t N>
+void write(int_t<N> data, bool ascending) {
+    do_write(key_encode(data, ascending));
+}
+
+/**
+ * @brief encode a field data to kvs binary representation
+ * @return
+ */
+std::size_t encode(accessor::record_ref ref, std::size_t offset, meta::field_type const& type, stream& dest) {
+    using kind = meta::field_type_kind;
+    switch(type.kind()) {
+        case kind::int4: dest.write<meta::field_type_traits<kind::int4>::runtime_type>(ref.get_value<meta::field_type_traits<kind::int4>::runtime_type>(offset)); break;
+        case kind::int8: dest.write<meta::field_type_traits<kind::int8>::runtime_type>(ref.get_value<meta::field_type_traits<kind::int8>::runtime_type>(offset)); break;
+        case kind::float4: dest.write<meta::field_type_traits<kind::float4>::runtime_type>(ref.get_value<meta::field_type_traits<kind::float4>::runtime_type>(offset)); break;
+        case kind::float8: dest.write<meta::field_type_traits<kind::float8>::runtime_type>(ref.get_value<meta::field_type_traits<kind::float8>::runtime_type>(offset)); break;
+        case kind::character: dest.write<meta::field_type_traits<kind::character>::runtime_type>(ref.get_value<meta::field_type_traits<kind::character>::runtime_type>(offset)); break;
+        default:
+            fail();
+    }
+}
+
+std::size_t decode(stream& src, meta::field_type const& type, accessor::record_ref ref, std::size_t offset, memory::paged_memory_resource* resource = nullptr) {
+    using kind = meta::field_type_kind;
+    switch(type.kind()) {
+        case kind::int4: ref.set_value<meta::field_type_traits<kind::int4>::runtime_type>(offset, src.read<meta::field_type_traits<kind::int4>::runtime_type>()); break;
+        case kind::int8: ref.set_value<meta::field_type_traits<kind::int8>::runtime_type>(offset, src.read<meta::field_type_traits<kind::int8>::runtime_type>()); break;
+        case kind::float4: ref.set_value<meta::field_type_traits<kind::float4>::runtime_type>(offset, src.read<meta::field_type_traits<kind::float4>::runtime_type>()); break;
+        case kind::float8: ref.set_value<meta::field_type_traits<kind::float8>::runtime_type>(offset, src.read<meta::field_type_traits<kind::float8>::runtime_type>()); break;
+        case kind::character: ref.set_value<meta::field_type_traits<kind::character>::runtime_type>(offset, src.read<meta::field_type_traits<kind::character>::runtime_type>(resource)); break;
+        default:
+            fail();
+    }
+}
+
+}
+
---
+++ test/jogasaki/kvs/coder_test.cpp
@@ -0,0 +1,285 @@
+/*
+ * Copyright 2018-2020 tsurugi project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <jogasaki/kvs/coder.h>
+
+#include <string>
+
+#include <takatori/util/object_creator.h>
+#include <gtest/gtest.h>
+
+#include <jogasaki/test_root.h>
+#include <jogasaki/kvs/transaction.h>
+#include <jogasaki/kvs/storage.h>
+#include <jogasaki/kvs/iterator.h>
+#include <jogasaki/kvs/environment.h>
+
+#include <jogasaki/mock_memory_resource.h>
+
+namespace jogasaki::kvs {
+
+using namespace executor;
+using namespace accessor;
+using namespace takatori::util;
+using namespace std::string_view_literals;
+using namespace std::string_literals;
+
+using namespace jogasaki::memory;
+using namespace boost::container::pmr;
+
+class coder_test : public test_root {
+public:
+    static void SetUpTestCase() {}
+    coder_test() {}
+private:
+};
+
+TEST_F(coder_test, simple) {
+    std::string buf(100, 0);
+    kvs::stream s{buf};
+    int_t<32> i32{1};
+    int_t<64> i64{2};
+    float_t<32> f32{3};
+    float_t<64> f64{4};
+    mock_memory_resource resource{};
+    accessor::text txt{&resource, "ABC"sv};
+    accessor::text txt2{&resource, "ABC"sv};
+    s.write(i32, true);
+    s.write(f32, true);
+    s.write(i64, true);
+    s.write(f64, true);
+    s.write(txt, true);
+
+    s.reset();
+
+    ASSERT_EQ(i32, s.read<std::int32_t>(true));
+    ASSERT_EQ(f32, s.read<float>(true));
+    ASSERT_EQ(i64, s.read<std::int64_t>(true));
+    ASSERT_EQ(f64, s.read<double>(true));
+    ASSERT_EQ(txt2, s.read<accessor::text>(&resource, true));
+}
+
+TEST_F(coder_test, descendant) {
+    std::string buf(100, 0);
+    kvs::stream s{buf};
+    int_t<32> i32{1};
+    int_t<64> i64{2};
+    float_t<32> f32{3};
+    float_t<64> f64{4};
+    mock_memory_resource resource{};
+    accessor::text txt{&resource, "ABC"sv};
+    accessor::text txt2{&resource, "ABC"sv};
+    s.write(i32, false);
+    s.write(f32, false);
+    s.write(i64, false);
+    s.write(f64, false);
+    s.write(txt, false);
+
+    s.reset();
+
+    ASSERT_EQ(i32, s.read<std::int32_t>(false));
+    ASSERT_EQ(f32, s.read<float>(false));
+    ASSERT_EQ(i64, s.read<std::int64_t>(false));
+    ASSERT_EQ(f64, s.read<double>(false));
+    ASSERT_EQ(txt, s.read<accessor::text>(&resource, false));
+}
+
+TEST_F(coder_test, i32_asc) {
+    std::string buf(100, 0);
+    kvs::stream s{buf};
+    int_t<32> i1{2};
+    int_t<32> i2{-2};
+    s.write(i1, true);
+    s.write(i2, true);
+
+    s.reset();
+
+    ASSERT_EQ(i1, s.read<std::int32_t>(true));
+    ASSERT_EQ(i2, s.read<std::int32_t>(true));
+    EXPECT_EQ('\x80', buf[0]);
+    EXPECT_EQ('\x00', buf[1]);
+    EXPECT_EQ('\x00', buf[2]);
+    EXPECT_EQ('\x02', buf[3]);
+    EXPECT_EQ('\x7F', buf[4]);
+    EXPECT_EQ('\xFF', buf[5]);
+    EXPECT_EQ('\xFF', buf[6]);
+    EXPECT_EQ('\xFE', buf[7]);
+}
+
+TEST_F(coder_test, i32_desc) {
+    std::string buf(100, 0);
+    kvs::stream s{buf};
+    int_t<32> i1{2};
+    int_t<32> i2{-2};
+    s.write(i1, false);
+    s.write(i2, false);
+
+    s.reset();
+
+    ASSERT_EQ(i1, s.read<std::int32_t>(false));
+    ASSERT_EQ(i2, s.read<std::int32_t>(false));
+    EXPECT_EQ('\x7F', buf[0]);
+    EXPECT_EQ('\xFF', buf[1]);
+    EXPECT_EQ('\xFF', buf[2]);
+    EXPECT_EQ('\xFD', buf[3]);
+    EXPECT_EQ('\x80', buf[4]);
+    EXPECT_EQ('\x00', buf[5]);
+    EXPECT_EQ('\x00', buf[6]);
+    EXPECT_EQ('\x01', buf[7]);
+}
+
+TEST_F(coder_test, i64_asc) {
+    std::string buf(100, 0);
+    kvs::stream s{buf};
+    int_t<64> i1{2};
+    int_t<64> i2{-2};
+    s.write(i1, true);
+    s.write(i2, true);
+
+    s.reset();
+
+    ASSERT_EQ(i1, s.read<std::int64_t>(true));
+    ASSERT_EQ(i2, s.read<std::int64_t>(true));
+    EXPECT_EQ('\x80', buf[0]);
+    EXPECT_EQ('\x00', buf[1]);
+    EXPECT_EQ('\x00', buf[2]);
+    EXPECT_EQ('\x00', buf[3]);
+    EXPECT_EQ('\x00', buf[4]);
+    EXPECT_EQ('\x00', buf[5]);
+    EXPECT_EQ('\x00', buf[6]);
+    EXPECT_EQ('\x02', buf[7]);
+
+    EXPECT_EQ('\x7F', buf[8]);
+    EXPECT_EQ('\xFF', buf[9]);
+    EXPECT_EQ('\xFF', buf[10]);
+    EXPECT_EQ('\xFF', buf[11]);
+    EXPECT_EQ('\xFF', buf[12]);
+    EXPECT_EQ('\xFF', buf[13]);
+    EXPECT_EQ('\xFF', buf[14]);
+    EXPECT_EQ('\xFE', buf[15]);
+}
+
+TEST_F(coder_test, f32_asc) {
+    std::string buf(100, 0);
+    kvs::stream s{buf};
+    float_t<32> f1{2};
+    float_t<32> f2{-2};
+    s.write(f1, true);
+    s.write(f2, true);
+
+    s.reset();
+
+    ASSERT_EQ(f1, s.read<float>(true));
+    ASSERT_EQ(f2, s.read<float>(true));
+    EXPECT_EQ('\xC0', buf[0]);
+    EXPECT_EQ('\x00', buf[1]);
+    EXPECT_EQ('\x00', buf[2]);
+    EXPECT_EQ('\x00', buf[3]);
+    EXPECT_EQ('\x3F', buf[4]);
+    EXPECT_EQ('\xFF', buf[5]);
+    EXPECT_EQ('\xFF', buf[6]);
+    EXPECT_EQ('\xFF', buf[7]);
+}
+
+TEST_F(coder_test, f32_desc) {
+    std::string buf(100, 0);
+    kvs::stream s{buf};
+    float_t<32> f1{2};
+    float_t<32> f2{-2};
+    s.write(f1, false);
+    s.write(f2, false);
+
+    s.reset();
+
+    ASSERT_EQ(f1, s.read<float>(false));
+    ASSERT_EQ(f2, s.read<float>(false));
+    EXPECT_EQ('\x3F', buf[0]);
+    EXPECT_EQ('\xFF', buf[1]);
+    EXPECT_EQ('\xFF', buf[2]);
+    EXPECT_EQ('\xFF', buf[3]);
+    EXPECT_EQ('\xC0', buf[4]);
+    EXPECT_EQ('\x00', buf[5]);
+    EXPECT_EQ('\x00', buf[6]);
+    EXPECT_EQ('\x00', buf[7]);
+}
+
+TEST_F(coder_test, float_nan) {
+    std::string buf(100, 0);
+    kvs::stream s{buf};
+    float_t<32> f1{std::nanf("1")};
+    float_t<32> f2{std::nanf("2")};
+    float_t<64> f3{std::nan("3")};
+    float_t<64> f4{std::nan("4")};
+    s.write(f1, true);
+    s.write(f2, false);
+    s.write(f3, true);
+    s.write(f4, false);
+
+    s.reset();
+
+    ASSERT_TRUE(std::isnan(s.read<float>(true)));
+    ASSERT_TRUE(std::isnan(s.read<float>(false)));
+    ASSERT_TRUE(std::isnan(s.read<double>(true)));
+    ASSERT_TRUE(std::isnan(s.read<double>(false)));
+}
+
+TEST_F(coder_test, f64_asc) {
+    std::string buf(100, 0);
+    kvs::stream s{buf};
+    float_t<64> f1{2};
+    float_t<64> f2{-2};
+    s.write(f1, true);
+    s.write(f2, true);
+
+    s.reset();
+
+    ASSERT_EQ(f1, s.read<double>(true));
+    ASSERT_EQ(f2, s.read<double>(true));
+    EXPECT_EQ('\xC0', buf[0]);
+    EXPECT_EQ('\x00', buf[1]);
+    EXPECT_EQ('\x00', buf[2]);
+    EXPECT_EQ('\x00', buf[3]);
+    EXPECT_EQ('\x00', buf[4]);
+    EXPECT_EQ('\x00', buf[5]);
+    EXPECT_EQ('\x00', buf[6]);
+    EXPECT_EQ('\x00', buf[7]);
+    EXPECT_EQ('\x3F', buf[8]);
+    EXPECT_EQ('\xFF', buf[9]);
+    EXPECT_EQ('\xFF', buf[10]);
+    EXPECT_EQ('\xFF', buf[11]);
+    EXPECT_EQ('\xFF', buf[12]);
+    EXPECT_EQ('\xFF', buf[13]);
+    EXPECT_EQ('\xFF', buf[14]);
+    EXPECT_EQ('\xFF', buf[15]);
+}
+
+TEST_F(coder_test, text) {
+    std::string buf(100, 0);
+    kvs::stream s{buf};
+    mock_memory_resource resource{};
+    accessor::text txt{&resource, "ABC"sv};
+    s.write(txt, true);
+
+    s.reset();
+
+    ASSERT_EQ(txt, s.read<accessor::text>(&resource, true));
+    EXPECT_EQ('\x80', buf[0]);
+    EXPECT_EQ('\x03', buf[1]);
+    EXPECT_EQ('A', buf[2]);
+    EXPECT_EQ('B', buf[3]);
+    EXPECT_EQ('C', buf[4]);
+}
+}
+

cfe5b2363b4913c485bff508d82292b01a2494d9
Ryoji Kurosawa <kurosawa@nautilus-technologies.com>
2021-01-20 22:02:47 +0900

    client_cli : reorg control flow

--- examples/client_cli/main.cpp
+++ examples/client_cli/main.cpp
@@ -27,17 +27,23 @@ namespace jogasaki::client_cli {
 using namespace std::string_literals;
 using namespace std::string_view_literals;
 
-static int run() {
-    auto env = jogasaki::api::create_environment();
-    env->initialize();
-    auto cfg = std::make_shared<configuration>();
-    cfg->prepare_benchmark_tables(true);
-    auto db = jogasaki::api::create_database(cfg);
-    db->start();
-
-    std::unique_ptr<api::result_set> rs{};
+static int prepare_data(api::database& db) {
     std::string insert_warehouse{"INSERT INTO WAREHOUSE (w_id, w_name, w_street_1, w_street_2, w_city, w_state, w_zip, w_tax, w_ytd) VALUES (1, 'fogereb', 'byqosjahzgrvmmmpglb', 'kezsiaxnywrh', 'jisagjxblbmp', 'ps', '694764299', 0.12, 3000000.00)"};
     std::string insert_customer{ "INSERT INTO CUSTOMER (c_id, c_d_id, c_w_id, c_first, c_middle, c_last, c_street_1, c_street_2, c_city, c_state, c_zip, c_phone, c_since, c_credit, c_credit_lim, c_discount, c_balance, c_data, c_ytd_payment, c_payment_cnt, c_delivery_cnt)  VALUES (1, 1, 1, 'pmdeqxrbgs', 'OE', 'BARBARBAR', 'zlaoknusaxfhasce', 'sagjvpdsyzbhsvnhwzxe', 'adftkgtros', 'qd', '827402212', '8700969702524002', '1973-12-12', 'BC', 50000.00, 0.05, -9.99, 'posxrsroejldsyoyirjofkqsycnbjoalxfkgipoogepnuwmagaxcopincpbfhwercrohqxygjjxhamineoraxkzrirkafmmjkcbkafvnqfzonsdcccijdzqlbywgcgbovpmmjcapfmfqbjnfejaqmhqqtxjayvowuujxqmzvisjghpjpynbamdhvvjncvgzstpvqeeakdpwkjmircrfysmwbbbkzbzefldktqfeubcbcjgdjsjtkcomuhqdazqmgpukiyawmqgyzkciwrxfswnegkrofklawoxypehzzztouvokzhshawbbdkasynuixskxmauxuapnkemytcrchqhvjqhntkvkmgezotza', 10.00, 1, 0)"};
+
+    if(auto res = db.execute(insert_warehouse); !res) {
+        db.stop();
+        return 1;
+    }
+    if(auto res = db.execute(insert_customer); !res) {
+        db.stop();
+        return 1;
+    }
+    return 0;
+}
+
+static int query(api::database& db) {
+    std::unique_ptr<api::result_set> rs{};
     std::string select{
         "SELECT w_tax, c_discount, c_last, c_credit FROM WAREHOUSE, CUSTOMER "
         "WHERE w_id = 1 "
@@ -45,17 +51,10 @@ static int run() {
         "c_d_id = 1 AND "
         "c_id = 1 "
     };
-    std::string sql {"select * from CUSTOMER c JOIN WAREHOUSE w ON c.c_w_id = w.w_id"};
-    if(auto res = db->execute(insert_warehouse); !res) {
-        db->stop();
-        return 1;
-    }
-    if(auto res = db->execute(insert_customer); !res) {
-        db->stop();
-        return 1;
-    }
-    if(auto res = db->execute(sql, rs); !res || !rs) {
-        db->stop();
+//    std::string sql {"select * from CUSTOMER c JOIN WAREHOUSE w ON c.c_w_id = w.w_id"};
+    std::string sql {"select * from CUSTOMER c"};
+    if(auto res = db.execute(sql, rs); !res || !rs) {
+        db.stop();
         return 1;
     }
     auto it = rs->iterator();
@@ -66,6 +65,18 @@ static int run() {
         LOG(INFO) << ss.str();
     }
     rs->close();
+    return 0;
+}
+
+static int run() {
+    auto env = jogasaki::api::create_environment();
+    env->initialize();
+    auto cfg = std::make_shared<configuration>();
+    cfg->prepare_benchmark_tables(true);
+    auto db = jogasaki::api::create_database(cfg);
+    db->start();
+    prepare_data(*db);
+    query(*db);
     db->stop();
     return 0;
 }

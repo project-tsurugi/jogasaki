7aefb97b3d1aa74bcbfc4d42cb20d35a48c7700e
Ryoji Kurosawa <kurosawa@nautilus-technologies.com>
2021-10-12 22:55:32 +0900

    move endpoint test mocks from tateyama

---
+++ mock/tateyama/api/endpoint/mock/endpoint_impls.cpp
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2018-2020 tsurugi project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <tateyama/api/endpoint/mock/endpoint_impls.h>
+
+#include <memory>
+#include <regex>
+
+namespace tateyama::api::endpoint::mock {
+
+using namespace std::literals::string_literals;
+using namespace std::string_view_literals;
+
+test_writer::test_writer(char* data, std::size_t capacity) :
+    data_(data), capacity_(capacity)
+{}
+
+status test_writer::write(char const* data, std::size_t length) {
+    std::memcpy(data_ + size_, data, length);
+    size_ += length;
+    return status::ok;
+}
+
+status test_writer::commit() {
+    return status::ok;
+}
+
+test_request::test_request(std::string_view payload) :
+    payload_(payload)
+{}
+
+std::string_view test_request::payload() const {
+    return payload_;
+}
+
+status test_channel::acquire(writer*& buf) {
+    auto& s = buffers_.emplace_back(std::make_shared<fixed_buffer_writer<100>>());
+    buf = s.get();
+    return status::ok;
+}
+
+status test_channel::release(writer& buf) {
+    (void)buf;
+//        for(auto it=buffers_.begin(); it != buffers_.end(); ++it) {
+//            if (it->get() == std::addressof(buf)) {
+//                buffers_.erase(it);
+//                break;
+//            }
+//        }
+    return status::ok;
+}
+
+void test_response::code(response_code code) {
+    code_ = code;
+}
+
+status test_response::body(std::string_view body) {
+    body_.assign(body);
+    completed_ = true;
+    return status::ok;
+}
+
+status test_response::body_head(std::string_view body_head) {
+    body_head_.assign(body_head);
+    return status::ok;
+}
+
+status test_response::acquire_channel(std::string_view name, data_channel*& ch) {
+    (void) name;
+    channel_ = std::make_unique<test_channel>();
+    ch = channel_.get();
+    return status::ok;
+}
+
+status test_response::release_channel(data_channel& ch) {
+    (void)ch;
+    return status::ok;
+}
+
+bool test_response::completed() {
+    return completed_;
+}
+
+status test_response::close_session() {
+    return status::ok;
+}
+
+}
---
+++ mock/tateyama/api/endpoint/mock/endpoint_impls.h
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2018-2020 tsurugi project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <tateyama/api/endpoint/writer.h>
+#include <tateyama/api/endpoint/request.h>
+#include <tateyama/api/endpoint/response.h>
+#include <tateyama/api/endpoint/data_channel.h>
+
+#include <memory>
+#include <regex>
+
+namespace tateyama::api::endpoint::mock {
+
+using namespace std::literals::string_literals;
+using namespace std::string_view_literals;
+
+class test_writer : public writer {
+
+public:
+    test_writer() = default;
+
+    test_writer(char* data, std::size_t capacity);
+
+    status write(char const* data, std::size_t length) override;
+
+    status commit() override;
+
+    char* data_{};  //NOLINT
+    std::size_t capacity_{};  //NOLINT
+    std::size_t size_{};  //NOLINT
+};
+
+template<std::size_t Size>
+class fixed_buffer_writer : public test_writer {
+public:
+    fixed_buffer_writer() {
+        data_ = array_.data();
+        capacity_ = array_.size();
+    }
+
+private:
+    std::array<char, Size> array_{};
+};
+
+class test_request : public request {
+public:
+    test_request() = default;
+
+    explicit test_request(std::string_view payload);
+
+    [[nodiscard]] std::string_view payload() const override;
+
+    std::string payload_{};  //NOLINT
+};
+
+class test_channel : public data_channel {
+public:
+    test_channel() = default;
+
+    status acquire(writer*& buf) override;
+
+    status release(writer& buf) override;
+
+    std::vector<std::shared_ptr<test_writer>> buffers_{};  //NOLINT
+};
+
+class test_response : public response {
+public:
+
+    void code(response_code code) override;
+
+    status body(std::string_view body) override;
+
+    status body_head(std::string_view body_head) override;
+
+    status acquire_channel(std::string_view name, data_channel*& ch) override;
+
+    status release_channel(data_channel& ch) override;
+
+    bool completed();
+
+    status close_session() override;;
+    std::string body_{};  //NOLINT
+    std::string body_head_{};  //NOLINT
+    std::unique_ptr<test_channel> channel_{};  //NOLINT
+    std::string message_{};  //NOLINT
+    response_code code_{response_code::unknown};  //NOLINT
+    bool completed_{};  //NOLINT
+};
+
+}

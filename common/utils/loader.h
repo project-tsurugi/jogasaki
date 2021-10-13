c7c759f217bd9b32bb2a528be6d9ca8a697a7953
Ryoji Kurosawa <kurosawa@nautilus-technologies.com>
2021-10-14 08:27:17 +0900

    import jogasaki related files from tateyama

---
+++ common/utils/loader.h
@@ -0,0 +1,202 @@
+/*
+ * Copyright 2018-2020 tsurugi project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <string_view>
+#include <atomic>
+#include <memory>
+#include <dlfcn.h>
+
+#include <glog/logging.h>
+#include <takatori/util/fail.h>
+
+#include <jogasaki/api.h>
+
+#include <tateyama/status.h>
+#include <tateyama/api/server/service.h>
+
+#include <cstring>
+
+#ifndef JOGASAKI_LIBRARY_NAME
+#error missing jogasaki library name
+#endif
+// a trick to convert defined value to char literal
+#define to_string_1(s) #s //NOLINT
+#define to_string_2(s) to_string_1(s) //NOLINT
+static constexpr auto jogasaki_library_name = to_string_2(JOGASAKI_LIBRARY_NAME); //NOLINT
+
+namespace jogasaki {
+class configuration;
+namespace api {
+class environment;
+class database;
+}
+}
+
+namespace tateyama::utils {
+
+using takatori::util::fail;
+
+namespace details {
+
+/**
+ * @brief dynamic library loader
+ * @details
+ */
+class loader {
+public:
+    loader(std::string_view filename, int flags) :
+        filename_(filename),
+        flags_(flags),
+        handle_(nullptr)
+    {
+        open();
+    }
+
+    loader() {
+        close();
+    }
+
+    loader(loader const& src) = delete;
+    loader(loader&&) = delete;
+
+    loader& operator = (loader const&) = delete;
+    loader& operator = (loader&&) = delete;
+
+    void* lookup(std::string_view symbol) {
+        void *fn = dlsym(handle_, symbol.data());
+        if (nullptr == fn) {
+            throw_exception(dlerror());
+        }
+        return fn;
+    }
+
+private:
+    std::string filename_;
+    int flags_;
+    void* handle_;
+    std::string error_{};
+
+    void open() {
+        handle_ = dlopen(filename_.c_str(), flags_);
+        if (nullptr == handle_) {
+            throw_exception(dlerror());
+        }
+        dlerror(); // reset error
+    }
+    void close() noexcept {
+        if (nullptr != handle_) {
+            dlclose(handle_);
+            handle_ = nullptr;
+        }
+    }
+    void throw_exception(const char* msg) {
+        error_.assign(msg);
+        throw std::runtime_error(error_);
+    }
+
+};
+
+loader& get_loader() {
+    static loader ldr(jogasaki_library_name, RTLD_NOW);
+    return ldr;
+}
+
+}
+
+/**
+ * @brief load and create environment
+ * @details load necessary SQL engine libraries and create environment.
+ * This will initialize the environment for SQL engine. Call this first before start using any other jogasaki functions.
+ * @note TODO revisit when components boundary is updated
+ * @warning ASAN and dlopen has compatibility issue (https://bugs.llvm.org/show_bug.cgi?id=27790).
+ * Specify install prefix to LD_LIBRARY_PATH when ASAN is used (e.g. on Debug build).
+ */
+inline std::shared_ptr<jogasaki::api::environment> create_environment() {
+    auto& ldr = details::get_loader();
+    jogasaki::api::environment* (*creater)(){};
+    void (*deleter)(jogasaki::api::environment*){};
+    try {
+        creater = reinterpret_cast<decltype(creater)>(ldr.lookup("new_environment"));  //NOLINT
+        deleter = reinterpret_cast<decltype(deleter)> (ldr.lookup("delete_environment"));  //NOLINT
+        return std::shared_ptr<jogasaki::api::environment>{creater(), [=](jogasaki::api::environment* ptr) { deleter(ptr); }};
+    } catch(const std::runtime_error& e) {
+        LOG(ERROR) << e.what();
+        fail();
+    }
+}
+
+
+/**
+ * @brief load and create database
+ * @details load necessary SQL engine libraries and create database.
+ * @param cfg the configuration used to create the database
+ * @return the database object
+ * @note TODO revisit when components boundary is updated
+ * @warning ASAN and dlopen has compatibility issue (https://bugs.llvm.org/show_bug.cgi?id=27790).
+ * Specify install prefix to LD_LIBRARY_PATH when ASAN is used (e.g. on Debug build).
+ */
+inline std::shared_ptr<jogasaki::api::database> create_database(jogasaki::configuration* cfg) {
+    auto& ldr = details::get_loader();
+    jogasaki::api::database* (*creater)(jogasaki::configuration*){};
+    void (*deleter)(jogasaki::api::database*){};
+    try {
+        creater = reinterpret_cast<decltype(creater)>(ldr.lookup("new_database"));  //NOLINT
+        deleter = reinterpret_cast<decltype(deleter)> (ldr.lookup("delete_database"));  //NOLINT
+        return std::shared_ptr<jogasaki::api::database>{creater(cfg), [=](jogasaki::api::database* ptr) { deleter(ptr); }};
+    } catch(const std::runtime_error& e) {
+        LOG(ERROR) << e.what();
+        fail();
+    }
+}
+
+
+/**
+ * @brief load and create application
+ * @details load necessary SQL engine libraries and create application
+ * @param db the database used by the application
+ * @return the application object
+ * @note TODO revisit when components boundary is updated
+ * @warning ASAN and dlopen has compatibility issue (https://bugs.llvm.org/show_bug.cgi?id=27790).
+ * Specify install prefix to LD_LIBRARY_PATH when ASAN is used (e.g. on Debug build).
+ */
+inline std::shared_ptr<tateyama::api::server::service> create_application(jogasaki::api::database* db) {
+    auto& ldr = details::get_loader();
+    tateyama::api::server::service* (*creater)(jogasaki::api::database*){};
+    void (*deleter)(tateyama::api::server::service*){};
+    try {
+        creater = reinterpret_cast<decltype(creater)>(ldr.lookup("new_application"));  //NOLINT
+        deleter = reinterpret_cast<decltype(deleter)> (ldr.lookup("delete_application"));  //NOLINT
+        return std::shared_ptr<tateyama::api::server::service>{creater(db), [=](tateyama::api::server::service* ptr) {
+            deleter(ptr);
+        }};
+    } catch(const std::runtime_error& e) {
+        LOG(ERROR) << e.what();
+        fail();
+    }
+}
+
+}
+
+
+
+
+
+
+
+
+
+
---
+++ examples/service_cli/main.cpp
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2018-2019 tsurugi project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <iostream>
+#include <vector>
+#include <memory>
+
+#include <glog/logging.h>
+#include <takatori/util/fail.h>
+#include <takatori/util/downcast.h>
+
+#include "../common/utils/loader.h"
+
+#include <jogasaki/api.h>
+
+DEFINE_bool(single_thread, false, "Whether to run on serial scheduler");  //NOLINT
+DEFINE_bool(work_sharing, false, "Whether to use on work sharing scheduler when run parallel");  //NOLINT
+DEFINE_int32(thread_count, 1, "Number of threads");  //NOLINT
+DEFINE_bool(core_affinity, true, "Whether threads are assigned to cores");  //NOLINT
+DEFINE_int32(initial_core, 1, "initial core number, that the bunch of cores assignment begins with");  //NOLINT
+DEFINE_bool(minimum, false, "run with minimum amount of data");  //NOLINT
+DEFINE_bool(assign_numa_nodes_uniformly, true, "assign cores uniformly on all numa nodes - setting true automatically sets core_affinity=true");  //NOLINT
+DEFINE_bool(debug, false, "debug mode");  //NOLINT
+DEFINE_bool(explain, false, "explain the execution plan");  //NOLINT
+DEFINE_int32(partitions, 10, "Number of partitions per process");  //NOLINT
+DEFINE_bool(steal, false, "Enable stealing for task scheduling");  //NOLINT
+DEFINE_bool(auto_commit, true, "Whether to commit when finishing each statement.");  //NOLINT
+DEFINE_bool(prepare_data, false, "Whether to prepare a few records in the storages");  //NOLINT
+
+namespace tateyama::service_cli {
+
+using namespace std::string_literals;
+using namespace std::string_view_literals;
+
+class cli {
+    std::shared_ptr<jogasaki::api::database> db_{};
+public:
+    int run(std::string_view sql, std::shared_ptr<jogasaki::configuration> cfg) {
+        auto env = tateyama::utils::create_environment();
+        db_ = tateyama::utils::create_database(cfg.get());
+        db_->start();
+        auto app = tateyama::utils::create_application(db_.get());
+
+        jogasaki::api::statement_handle stmt{};
+        auto p = db_->prepare(sql, stmt);
+        (void)p;
+        db_->destroy_statement(stmt);
+        db_->stop();
+        return 0;
+    }
+};
+
+}  // namespace
+
+extern "C" int main(int argc, char* argv[]) {
+    // ignore log level
+    if (FLAGS_log_dir.empty()) {
+        FLAGS_logtostderr = true;
+    }
+    google::InitGoogleLogging("service cli");
+    google::InstallFailureSignalHandler();
+    gflags::SetUsageMessage("service cli");
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+    tateyama::service_cli::cli e{};
+    auto cfg = std::make_shared<jogasaki::configuration>();
+    cfg->single_thread(true);
+    std::string_view source { argv[1] }; // NOLINT
+    try {
+        e.run(source, cfg);  // NOLINT
+    } catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return -1;
+    }
+    return 0;
+}

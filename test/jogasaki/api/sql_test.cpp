fc37fa8c49911f23df1a9f484f26bdf0e76c131a
Ryoji Kurosawa <kurosawa@nautilus-technologies.com>
2022-12-29 19:51:28 +0900

    add outer joing testcases

--- test/jogasaki/api/sql_test.cpp
+++ test/jogasaki/api/sql_test.cpp
@@ -83,6 +83,83 @@ TEST_F(sql_test, cross_join) {
     ASSERT_EQ(6, result.size());
 }
 
+TEST_F(sql_test, outer_join) {
+    execute_statement("create table L (C0 INT PRIMARY KEY, C1 INT)");
+    execute_statement("create table R (C0 INT PRIMARY KEY, C1 INT)");
+    execute_statement( "INSERT INTO L (C0, C1) VALUES (1, 1)");
+    execute_statement( "INSERT INTO L (C0, C1) VALUES (2, 2)");
+    execute_statement( "INSERT INTO L (C0, C1) VALUES (3, 3)");
+    execute_statement( "INSERT INTO R (C0, C1) VALUES (1, 1)");
+    execute_statement( "INSERT INTO R (C0, C1) VALUES (30, 3)");
+    execute_statement( "INSERT INTO R (C0, C1) VALUES (31, 3)");
+    execute_statement( "INSERT INTO R (C0, C1) VALUES (4, 4)");
+
+    {
+        std::vector<mock::basic_record> result{};
+        execute_query("SELECT * FROM L LEFT JOIN R ON L.C1=R.C1 ORDER BY L.C0, R.C0", result);
+        ASSERT_EQ(4, result.size());
+    }
+    {
+        std::vector<mock::basic_record> result{};
+        execute_query("SELECT * FROM R RIGHT JOIN L ON L.C1=R.C1 ORDER BY L.C0, R.C0", result);
+        ASSERT_EQ(4, result.size());
+    }
+}
+
+TEST_F(sql_test, full_outer_join) {
+    execute_statement("create table L (C0 INT PRIMARY KEY, C1 INT)");
+    execute_statement("create table R (C0 INT PRIMARY KEY, C1 INT)");
+    execute_statement( "INSERT INTO L (C0, C1) VALUES (1, 1)");
+    execute_statement( "INSERT INTO L (C0, C1) VALUES (2, 2)");
+    execute_statement( "INSERT INTO L (C0, C1) VALUES (3, 3)");
+    execute_statement( "INSERT INTO L (C0, C1) VALUES (50, 5)");
+    execute_statement( "INSERT INTO L (C0, C1) VALUES (51, 5)");
+    execute_statement( "INSERT INTO R (C0, C1) VALUES (1, 1)");
+    execute_statement( "INSERT INTO R (C0, C1) VALUES (30, 3)");
+    execute_statement( "INSERT INTO R (C0, C1) VALUES (31, 3)");
+    execute_statement( "INSERT INTO R (C0, C1) VALUES (4, 4)");
+    execute_statement( "INSERT INTO R (C0, C1) VALUES (5, 5)");
+
+    {
+        std::vector<mock::basic_record> result{};
+        execute_query("SELECT * FROM L FULL OUTER JOIN R ON L.C1=R.C1 ORDER BY L.C0, R.C0", result);
+        ASSERT_EQ(7, result.size());
+    }
+    {
+        std::vector<mock::basic_record> result{};
+        execute_query("SELECT * FROM R FULL OUTER JOIN L ON L.C1=R.C1 ORDER BY L.C0, R.C0", result);
+        ASSERT_EQ(7, result.size());
+    }
+}
+
+TEST_F(sql_test, update_by_part_of_primary_key) {
+    execute_statement( "INSERT INTO T20 (C0, C2, C4) VALUES (1, 100.0, '111')");
+    execute_statement( "UPDATE T20 SET C2=200.0 WHERE C0=1");
+    std::vector<mock::basic_record> result{};
+    execute_query("SELECT C0, C1, C2 FROM T20", result);
+    ASSERT_EQ(1, result.size());
+    auto& rec = result[0];
+    EXPECT_EQ(1, rec.get_value<std::int64_t>(0));
+    EXPECT_TRUE(rec.is_null(1));
+    EXPECT_DOUBLE_EQ(200.0, rec.get_value<double>(2));
+    EXPECT_FALSE(rec.is_null(2));
+}
+
+TEST_F(sql_test, update_primary_key) {
+    execute_statement( "INSERT INTO T0 (C0, C1) VALUES (1, 10.0)");
+    execute_statement( "INSERT INTO T0 (C0, C1) VALUES (2, 20.0)");
+    execute_statement( "UPDATE T0 SET C0=3, C1=30.0 WHERE C1=10.0");
+    wait_epochs(2);
+    std::vector<mock::basic_record> result{};
+    execute_query("SELECT C0, C1 FROM T0 ORDER BY C0", result);
+    ASSERT_EQ(2, result.size());
+    auto meta = result[0].record_meta();
+    EXPECT_EQ(2, result[0].get_value<std::int64_t>(0));
+    EXPECT_DOUBLE_EQ(20.0, result[0].get_value<double>(1));
+    EXPECT_EQ(3, result[1].get_value<std::int64_t>(0));
+    EXPECT_DOUBLE_EQ(30.0, result[1].get_value<double>(1));
+}
+
 TEST_F(sql_test, count_empty_records) {
     std::vector<mock::basic_record> result{};
     execute_query("SELECT COUNT(C1) FROM T0", result);

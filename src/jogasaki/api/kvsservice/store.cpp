7b3ef23df7002ca5d26ca5d0ede815cf463389ba
Katsuhiko Ohkubo <ohkubo@nautilus-technologies.com>
2023-06-27 14:34:25 +0900

    temporary set DUMMY_TX just for test

--- src/jogasaki/api/kvsservice/store.cpp
+++ src/jogasaki/api/kvsservice/store.cpp
@@ -23,7 +23,13 @@ store::store(std::shared_ptr<jogasaki::api::resource::bridge> const& bridge) :
     db_(dynamic_cast<jogasaki::api::impl::database*>(bridge->database())->kvs_db()->handle()){
 }
 
+#define DUMMY_TX // FIXME just for test only
+#ifdef DUMMY_TX
+static std::shared_ptr<transaction> dummy_tx = std::make_shared<transaction>(nullptr);
+#endif
+
 status store::begin_transaction(transaction_option const &, std::shared_ptr<transaction>& tx) {
+#ifndef DUMMY_TX
     // FIXME call sharksfin: transaction_begin
     sharksfin::TransactionControlHandle handle {};
     tx = std::make_shared<transaction>(handle);
@@ -32,24 +38,40 @@ status store::begin_transaction(transaction_option const &, std::shared_ptr<tran
     if (transactions_.insert(acc, tx->system_id())) {
         acc->second = tx;
     }
+#else
+    tx = dummy_tx;
+#endif
     return status::ok;
 }
 
-std::shared_ptr<transaction> store::find_transaction(std::uint64_t system_id) {
+std::shared_ptr<transaction> store::find_transaction(std::uint64_t
+#ifndef DUMMY_TX
+system_id
+#endif
+) {
+#ifndef DUMMY_TX
     decltype(transactions_)::accessor acc{};
     if (transactions_.find(acc, system_id)) {
         return acc->second;
     }
     return nullptr;
+#else
+    return dummy_tx;
+#endif
 }
 
-status store::dispose_transaction(std::uint64_t system_id) {
+status store::dispose_transaction(std::uint64_t
+#ifndef DUMMY_TX
+system_id
+#endif
+) {
     // FIXME call sharksfin: transaction_dispose
-    //
+#ifndef DUMMY_TX
     decltype(transactions_)::accessor acc{};
     if (transactions_.find(acc, system_id)) {
         transactions_.erase(acc);
     }
+#endif
     return status::ok;
 }
 }

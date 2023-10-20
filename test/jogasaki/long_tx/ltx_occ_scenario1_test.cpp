eeadcfad798ba0103021dd27e4be85a2651687d4
Ryoji Kurosawa <kurosawa@nautilus-technologies.com>
2023-10-20 09:15:04 +0900

    test: recover cc test scenario for ltx and occ

--- test/jogasaki/long_tx/ltx_occ_scenario1_test.cpp
+++ test/jogasaki/long_tx/ltx_occ_scenario1_test.cpp
@@ -44,7 +44,7 @@ using namespace jogasaki::scheduler;
 
 using takatori::util::unsafe_downcast;
 
-// regression testcase - starting ltx and then occ commit stopped sending new durability marker for the occ
+// regression testcase issue #390 - starting ltx and then occ commit stopped sending new durability marker for the occ
 class ltx_occ_scenario1_test :
     public ::testing::Test,
     public api_test_base {
@@ -69,23 +69,31 @@ public:
 
 using namespace std::string_view_literals;
 
-// TODO enable after fix
-TEST_F(ltx_occ_scenario1_test, DISABLED_never_durable) {
+TEST_F(ltx_occ_scenario1_test, never_durable) {
     utils::set_global_tx_option({false, true});
     execute_statement("create table T1(C0 INT NOT NULL PRIMARY KEY)");
     execute_statement("create table T2(C0 INT NOT NULL PRIMARY KEY)");
     auto ltx = utils::create_transaction(*db_, false, true, {"T1"});
-//    execute_statement("INSERT INTO T1 VALUES (0)", *ltx);
-    {
-//        auto f1 = std::async(std::launch::async, [&]() {
-            auto occ = utils::create_transaction(*db_);
-//            execute_statement("INSERT INTO T2 VALUES (100)", *occ);
-            ASSERT_EQ(status::ok, occ->commit());
-//        });
-//        f1.get();
-    }
-//    execute_statement("INSERT INTO T1 VALUES (10)", *ltx);
-//    ASSERT_EQ(status::ok, ltx->commit());
+    auto occ = utils::create_transaction(*db_);
+    ASSERT_EQ(status::ok, occ->commit());
+    ASSERT_EQ(status::ok, ltx->commit());
 }
 
+TEST_F(ltx_occ_scenario1_test, never_durable_commit_from_other_thread) {
+    utils::set_global_tx_option({false, true});
+    execute_statement("create table T1(C0 INT NOT NULL PRIMARY KEY)");
+    execute_statement("create table T2(C0 INT NOT NULL PRIMARY KEY)");
+    auto ltx = utils::create_transaction(*db_, false, true, {"T1"});
+    execute_statement("INSERT INTO T1 VALUES (0)", *ltx);
+    {
+        auto f1 = std::async(std::launch::async, [&]() {
+            auto occ = utils::create_transaction(*db_);
+            execute_statement("INSERT INTO T2 VALUES (100)", *occ);
+            ASSERT_EQ(status::ok, occ->commit());
+        });
+        f1.get();
+    }
+    execute_statement("INSERT INTO T1 VALUES (10)", *ltx);
+    ASSERT_EQ(status::ok, ltx->commit());
+}
 }

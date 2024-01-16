1eef99b054f8ee4a2cfb321fd174578bce5f9fc6
Ryoji Kurosawa <kurosawa@nautilus-technologies.com>
2024-01-17 08:03:50 +0900

    test: add create_index_test

---
+++ test/jogasaki/api/create_index_test.cpp
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2018-2023 Project Tsurugi.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <regex>
+#include <gtest/gtest.h>
+
+#include <takatori/util/downcast.h>
+
+#include <jogasaki/executor/common/graph.h>
+#include <jogasaki/scheduler/dag_controller.h>
+#include <jogasaki/data/any.h>
+
+#include <jogasaki/mock/basic_record.h>
+#include <jogasaki/utils/storage_data.h>
+#include <jogasaki/api/database.h>
+#include <jogasaki/api/impl/database.h>
+#include <jogasaki/api/result_set.h>
+#include <jogasaki/api/impl/record.h>
+#include <jogasaki/api/impl/record_meta.h>
+#include <jogasaki/meta/type_helper.h>
+#include <jogasaki/executor/tables.h>
+#include "api_test_base.h"
+#include <jogasaki/test_utils/secondary_index.h>
+#include <jogasaki/kvs/id.h>
+
+namespace jogasaki::testing {
+
+using namespace std::literals::string_literals;
+using namespace jogasaki;
+using namespace jogasaki::meta;
+using namespace jogasaki::model;
+using namespace jogasaki::executor;
+using namespace jogasaki::scheduler;
+using namespace jogasaki::mock;
+
+using date_v = takatori::datetime::date;
+using time_of_day_v = takatori::datetime::time_of_day;
+using time_point_v = takatori::datetime::time_point;
+using decimal_v = takatori::decimal::triple;
+using takatori::util::unsafe_downcast;
+using kind = meta::field_type_kind;
+using api::impl::get_impl;
+
+class create_index_test:
+    public ::testing::Test,
+    public api_test_base {
+
+public:
+    // change this flag to debug with explain
+    bool to_explain() override {
+        return false;
+    }
+
+    void SetUp() override {
+        auto cfg = std::make_shared<configuration>();
+        db_setup(cfg);
+    }
+
+    void TearDown() override {
+        db_teardown();
+    }
+};
+
+using namespace std::string_view_literals;
+
+TEST_F(create_index_test, create_secondary_blocked_by_existing_records) {
+    execute_statement("CREATE TABLE T (C0 INT NOT NULL PRIMARY KEY, C1 INT)");
+    execute_statement("INSERT INTO T VALUES(1,1)");
+    {
+        std::vector<mock::basic_record> result{};
+        execute_query("SELECT * FROM T", result);
+        ASSERT_EQ(1, result.size());
+        auto& rec = result[0];
+        EXPECT_EQ((create_nullable_record<kind::int4, kind::int4>(1,1)), result[0]);
+    }
+    test_stmt_err("CREATE INDEX I ON T (C1)", error_code::unsupported_runtime_feature_exception);
+}
+
+}

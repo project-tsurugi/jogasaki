e6cd6cdc68443671f6fe037a50bddf79d9eb6dcd
Nobuhiro Ban <ban@nautilus-technologies.com>
2024-04-29 19:02:21 +0900

    add missing include (for FTBFS with gcc-13)

--- include/jogasaki/status.h
+++ include/jogasaki/status.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2023 Project Tsurugi.
+ * Copyright 2018-2024 Project Tsurugi.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +16,7 @@
 #pragma once
 
 #include <cstddef>
+#include <cstdint>
 #include <cstdlib>
 #include <string_view>
 #include <ostream>

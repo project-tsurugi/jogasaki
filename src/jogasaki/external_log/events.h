91693c62c912294269baafee47d52528404ce2d8
Ryoji Kurosawa <kurosawa@nautilus-technologies.com>
2024-07-02 16:42:31 +0900

    use altimeter constants.h and set log level
    https://github.com/project-tsurugi/tsurugi-issues/issues/816
    https://github.com/project-tsurugi/tsurugi-issues/issues/817

--- src/jogasaki/external_log/events.h
+++ src/jogasaki/external_log/events.h
@@ -16,56 +16,7 @@
 #pragma once
 
 #include <string_view>
-
 namespace jogasaki::external_log {
-// log category list
-namespace log_category {
-static constexpr std::string_view event = "event";
-static constexpr std::string_view audit = "audit";
-} // namespace log_category
-
-// event log type list
-namespace log_type::event {
-static constexpr std::string_view session_start = "session_start";
-static constexpr std::string_view session_end = "session_end";
-static constexpr std::string_view tx_start = "tx_start";
-static constexpr std::string_view tx_end = "tx_end";
-static constexpr std::string_view stmt_start = "stmt_start";
-static constexpr std::string_view stmt_end = "stmt_end";
-static constexpr std::string_view stmt_explain = "stmt_explain";
-} // namespace log_type::event
-
-// event log item list
-namespace log_item::event {
-static constexpr std::string_view user = "user";
-static constexpr std::string_view dbname = "dbname";
-static constexpr std::string_view pid = "pid";
-static constexpr std::string_view remote_host = "remote_host";
-static constexpr std::string_view application_name = "application_name";
-static constexpr std::string_view session_id = "session_id";
-static constexpr std::string_view session_label = "session_label";
-static constexpr std::string_view tx_id = "tx_id";
-static constexpr std::string_view tx_type = "tx_type";
-static constexpr std::string_view job_id = "job_id";
-static constexpr std::string_view result = "result";
-static constexpr std::string_view state_code = "state_code";
-static constexpr std::string_view message = "message";
-static constexpr std::string_view statement = "statement";
-static constexpr std::string_view parameter = "parameter";
-static constexpr std::string_view fetched = "fetched";
-static constexpr std::string_view returned = "returned";
-static constexpr std::string_view inserted = "inserted";
-static constexpr std::string_view updated = "updated";
-static constexpr std::string_view deleted = "deleted";
-static constexpr std::string_view merged = "merged";
-static constexpr std::string_view data = "data";
-}
-
-// event log level list
-namespace log_level::event {
-static constexpr int error = 30;
-static constexpr int info = 50;
-} // namespace log_level::event
 
 // values for tx_type
 namespace tx_type_value {

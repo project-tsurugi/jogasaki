0c13ccce2c07fa1dccf671cbf37e3b0f24a25175
Ryoji Kurosawa <kurosawa@nautilus-technologies.com>
2024-07-05 16:27:38 +0900

    show new config. values in the log messages

--- src/jogasaki/api/impl/database.cpp
+++ src/jogasaki/api/impl/database.cpp
@@ -168,6 +168,8 @@ void dump_public_configurations(configuration const& cfg) {
     LOGCFG << "(dev_try_insert_on_upserting_secondary) " << cfg.try_insert_on_upserting_secondary() << " : whether to try insert first when INSERT OR REPLACE is exected for tables with secondary index";
     LOGCFG << "(dev_scan_concurrent_operation_as_not_found) " << cfg.scan_concurrent_operation_as_not_found() << " : whether scan to treat status::concurrent_operation as status::not_found";
     LOGCFG << "(dev_point_read_concurrent_operation_as_not_found) " << cfg.point_read_concurrent_operation_as_not_found() << " : whether point read to treat status::concurrent_operation as status::not_found";
+    LOGCFG << "(dev_compiler_support) " << cfg.compiler_support() << " : support level of sql compiler. 0: legacy, 1: new";
+    LOGCFG << "(dev_lowercase_regular_identifiers) " << cfg.lowercase_regular_identifiers() << " : whether to lowercase regular identifiers";
 }
 
 status database::start() {

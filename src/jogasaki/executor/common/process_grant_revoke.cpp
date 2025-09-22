c55f842f2b714bd06af3679bff82429312a3256a
Ryoji Kurosawa <kurosawa@nautilus-technologies.com>
2025-09-22 20:49:20 +0900

    feat(jogasaki): REVOKE with CURRENT_USER and `*`
    https://github.com/project-tsurugi/tsurugi-issues/issues/426

--- src/jogasaki/executor/common/process_grant_revoke.cpp
+++ src/jogasaki/executor/common/process_grant_revoke.cpp
@@ -23,6 +23,7 @@
 #include <utility>
 #include <boost/assert.hpp>
 
+#include <takatori/statement/details/table_authorization_entry.h>
 #include <takatori/util/string_builder.h>
 #include <yugawara/binding/extract.h>
 #include <yugawara/storage/basic_configurable_provider.h>
@@ -69,19 +70,16 @@ static std::string_view to_string_view(tateyama::api::server::user_type type) {
     std::abort();
 }
 
-bool process_grant_revoke(  //NOLINT(readability-function-cognitive-complexity)
+static bool check_grant_revoke_preconditions(  //NOLINT(readability-function-cognitive-complexity)
     bool grant,
     request_context& context,
-    std::vector<takatori::statement::details::table_privilege_element> const& elements
+    std::vector<takatori::statement::details::table_privilege_element> const& elements,
+    std::optional<std::string_view> const& current_user
 ) {
-    BOOST_ASSERT(context.storage_provider());  //NOLINT
-
-    // pre-condition check: verify that all target tables exist and acquire locks successfully
-
     auto& smgr = *global::storage_manager();
     auto& provider = *context.storage_provider();
-    for(auto&& e : elements) {
-        auto c = yugawara::binding::extract_shared<yugawara::storage::table>(e.table());
+    for(auto&& tpe : elements) {
+        auto c = yugawara::binding::extract_shared<yugawara::storage::table>(tpe.table());
         if (! c) {
             set_error(
                 context,
@@ -102,6 +100,45 @@ bool process_grant_revoke(  //NOLINT(readability-function-cognitive-complexity)
             return false;
         }
 
+        for(auto&& tae: tpe.authorization_entries()) {
+            if(tae.user_kind() == takatori::statement::authorization_user_kind::current_user) {
+                // `current_user` is allowed only when the user name is available
+                if (! current_user.has_value()) {
+                    set_error(
+                        context,
+                        error_code::value_evaluation_exception,
+                        "current_user value is not available",
+                        status::err_expression_evaluation_failure
+                    );
+                    return false;
+                }
+            }
+            if(tae.user_kind() == takatori::statement::authorization_user_kind::all_users) {
+                // `*` is allowed only when the user name is available
+                if (! current_user.has_value()) {
+                    set_error(
+                        context,
+                        error_code::value_evaluation_exception,
+                        "cannot revoke table privileges from all users when authentication mechanism is disabled",
+                        status::err_expression_evaluation_failure
+                    );
+                    return false;
+                }
+                // `*` is allowed for REVOKE ALL PRIVILEGES only
+                for(auto&& tpa : tae.privileges()) {
+                    if(tpa.action_kind() != takatori::statement::details::table_privilege_action::action_kind_type::control) {
+                        set_error(
+                            context,
+                            error_code::unsupported_runtime_feature_exception,
+                            "to revoke table privileges from all users, the privilege must be ALL PRIVILEGES",
+                            status::err_unsupported
+                        );
+                        return false;
+                    }
+                }
+            }
+        }
+
         // on any error during this for loop, the tx will be aborted in caller and the acquired lock will be released
         storage::storage_entry tid{};
         if(! acquire_table_lock(context, c->simple_name(), tid)) {
@@ -113,46 +150,176 @@ bool process_grant_revoke(  //NOLINT(readability-function-cognitive-complexity)
         // - admin users
         // - user with control privilege
         // - user with alter privilege (not supported yet)
-        if (context.req_info().request_source()) {
-            auto name = context.req_info().request_source()->session_info().username();
-            if(auto type = context.req_info().request_source()->session_info().user_type();
-               type != tateyama::api::server::user_type::administrator) {
-                auto sc = smgr.find_entry(tid);
-                assert_with_exception(sc != nullptr, c->simple_name());  // must exist as we locked successfully above
+        if (! context.req_info().request_source() || context.req_info().request_source()->session_info().user_type() ==
+            tateyama::api::server::user_type::administrator) {
+            // if request_source is null, this is in testcase
+            continue;
+        }
+        auto sc = smgr.find_entry(tid);
+        assert_with_exception(sc != nullptr, c->simple_name());  // must exist as we locked successfully above
 
-                // TODO change to alter when supported
-                if (name.has_value() && sc->allows_user_actions(name.value(), auth::action_set{auth::action_kind::control})) {
-                    if (grant) {
-                        // if grant, the privileges must be subset of user's own privileges
-                        // TODO uncomment below to enable check when alter is supported
-                        // if(! sc->allows(name.value(), from(e.default_privileges()))) {
-                        //     return false;
-                        // }
-                        // for(auto&& tae : e.authorization_entries()) {
-                        //     if(! sc->allows(name.value(), from(tae.privileges()))) {
-                        //         return false;
-                        //     }
-                        // }
-                    }
-                    continue;
-                }
+        // TODO change to alter when supported
+        if (current_user.has_value() &&
+            sc->allows_user_actions(current_user.value(), auth::action_set{auth::action_kind::control})) {
 
-                VLOG_LP(log_error) << "insufficient authorization user:\"" << (name.has_value() ? name.value() : "")
-                                   << "\" user_type:" << to_string_view(type);
-                set_error(
-                    context,
-                    error_code::permission_error,
-                    "insufficient authorization for the requested operation",
-                    status::err_illegal_operation
-                );
-                return false;
+            if (grant) {
+                // if grant, the privileges must be subset of user's own privileges
+                // TODO uncomment below to enable check when alter is supported
+                // if(! sc->allows(name.value(), from(e.default_privileges()))) {
+                //     return false;
+                // }
+                // for(auto&& tae : e.authorization_entries()) {
+                //     if(! sc->allows(name.value(), from(tae.privileges()))) {
+                //         return false;
+                //     }
+                // }
             }
+            continue;
+        }
+
+        VLOG_LP(log_error) << "insufficient authorization user:\"" << (current_user.has_value() ? current_user.value() : "")
+                           << "\" user_type:" << to_string_view(tateyama::api::server::user_type::standard);
+        set_error(
+            context,
+            error_code::permission_error,
+            "insufficient authorization for the requested operation",
+            status::err_illegal_operation
+        );
+        return false;
+    }
+    return true;
+}
+
+static std::string_view get_grantee(
+    takatori::statement::details::table_authorization_entry const& tae,
+    std::optional<std::string_view> const& current_user
+) {
+    std::string_view ret{};
+    switch(tae.user_kind()) {
+        case takatori::statement::authorization_user_kind::specified:
+            ret = tae.authorization_identifier() ;
+            break;
+        case takatori::statement::authorization_user_kind::current_user:
+            assert_with_exception(current_user.has_value(), tae.authorization_identifier());  // already checked above //NOLINT
+            ret = current_user.value();
+            break;
+        case takatori::statement::authorization_user_kind::all_users:
+            // for grant, all_users will not come here
+            break;
+    }
+    return ret;
+}
+
+static std::pair<auth::action_set, auth::authorized_users_action_set> calculate_public_and_authorized_actions(
+    takatori::statement::details::table_privilege_element const& tpe,
+    storage::impl::storage_control& sc,
+    bool grant,
+    std::optional<std::string_view> const& current_user
+) {
+    auth::action_set public_actions = sc.public_actions();
+    auth::authorized_users_action_set authorized_actions = sc.authorized_actions();
+
+    if (grant) {
+        public_actions.add_actions(from(tpe.default_privileges()));
+        for(auto&& tae : tpe.authorization_entries()) {
+            auto grantee = get_grantee(tae, current_user);
+            authorized_actions.add_user_actions(grantee, from(tae.privileges()));
+        }
+    } else {
+        public_actions.remove_actions(from(tpe.default_privileges()));
+        for(auto&& tae : tpe.authorization_entries()) {
+            if(tae.user_kind() == takatori::statement::authorization_user_kind::all_users) {
+                assert_with_exception(current_user.has_value(), tae.authorization_identifier());  // already checked above //NOLINT
+                public_actions.clear();
+                for(auto it = authorized_actions.begin(), end = authorized_actions.end(); it != end; ) {
+                    if (it->first != current_user.value()) {
+                        it = authorized_actions.erase(it);
+                        continue;
+                    }
+                    ++it;
+                }
+                continue;
+            }
+            // for specified or current_user
+            auto grantee = get_grantee(tae, current_user);
+            authorized_actions.remove_user_actions(grantee, from(tae.privileges()));
         }
     }
+    return {public_actions, authorized_actions};
+}
 
-    // pre-condition checked, let's make changes
-    // after this point, we expect no error occurs normally; if any error occurs, that is unexpected internal error
+static bool serialize_and_save(
+    request_context& context,
+    std::string_view table_name,
+    storage::impl::storage_control& sc,
+    auth::action_set const& public_actions,
+    auth::authorized_users_action_set const& authorized_actions
+) {
+    auto& provider = *context.storage_provider();
+    auto primary_index = provider.find_index(table_name);
+    assert_with_exception(primary_index != nullptr, table_name);
+    std::string storage{};
+    if(auto err = recovery::create_storage_option(
+           *primary_index,
+           storage,
+           utils::metadata_serializer_option{
+               false,
+               std::addressof(authorized_actions),
+               std::addressof(public_actions)
+           }
+       )) {
+        // error should not happen normally
+        set_error_info(context, err);
+        return false;
+    }
 
+    proto::metadata::storage::IndexDefinition idef{};
+    std::uint64_t v{};
+    if(auto err = recovery::validate_extract(storage, idef, v)) {
+        // should not happen normally
+        // we just created the option above
+        set_error_info(context, err);
+        return false;
+    }
+    sc.authorized_actions().clear();
+    from_authorization_list(idef.table_definition(), sc.authorized_actions());
+    sc.public_actions().clear();
+    auth::from_default_privilege(idef.table_definition(), sc.public_actions());
+
+    sharksfin::StorageOptions options{};
+    options.payload(std::move(storage));
+    auto stg = context.database()->get_or_create_storage(table_name);
+    if(! stg) {
+        // should not happen normally
+        set_error(
+            context,
+            error_code::target_not_found_exception,
+            string_builder{} << "Storage \"" << table_name << "\" not found" << string_builder::to_string,
+            status::err_not_found
+        );
+        return false;
+    }
+    if(auto res = stg->set_options(options);res != status::ok) {
+        // should not happen normally
+        // though this calls sharksfin, updating storage metadata should almost always succeed
+        set_error(
+            context,
+            error_code::sql_execution_exception,
+            string_builder{} << "failed to modify storage metadata. status:" << res << string_builder::to_string,
+            status::err_unknown
+        );
+        return false;
+    }
+    return true;
+}
+
+static bool reflect_grant_revoke(
+    bool grant,
+    request_context& context,
+    std::vector<takatori::statement::details::table_privilege_element> const& elements,
+    std::optional<std::string_view> const& current_user
+) {
+    auto& smgr = *global::storage_manager();
     for(auto&& tpe : elements) {
         auto& c = *yugawara::binding::extract_shared<yugawara::storage::table>(tpe.table());
 
@@ -165,77 +332,36 @@ bool process_grant_revoke(  //NOLINT(readability-function-cognitive-complexity)
         // durable one (in storage option), we first copy the in-memory metadata, serialize to create storage option,
         // and deserialize into the in-memory one again
 
-        auth::action_set public_actions = sc->public_actions();
-        auth::authorized_users_action_set authorized_actions = sc->authorized_actions();
-
-        if (grant) {
-            public_actions.add_actions(from(tpe.default_privileges()));
-            for(auto&& tae : tpe.authorization_entries()) {
-                authorized_actions.add_user_actions(tae.authorization_identifier(), from(tae.privileges()));
-            }
-        } else {
-            public_actions.remove_actions(from(tpe.default_privileges()));
-            for(auto&& tae : tpe.authorization_entries()) {
-                authorized_actions.remove_user_actions(tae.authorization_identifier(), from(tae.privileges()));
-            }
-        }
-
-        auto primary_index = provider.find_index(c.simple_name());
-        assert_with_exception(primary_index != nullptr, c.simple_name());
-        std::string storage{};
-        if(auto err = recovery::create_storage_option(
-               *primary_index,
-               storage,
-               utils::metadata_serializer_option{
-                   false,
-                   std::addressof(authorized_actions),
-                   std::addressof(public_actions)
-               }
-           )) {
-            // error should not happen normally
-            set_error_info(context, err);
-            return false;
-        }
-
-        proto::metadata::storage::IndexDefinition idef{};
-        std::uint64_t v{};
-        if(auto err = recovery::validate_extract(storage, idef, v)) {
-            // should not happen normally
-            // we just created the option above
-            set_error_info(context, err);
-            return false;
-        }
-        sc->authorized_actions().clear();
-        from_authorization_list(idef.table_definition(), sc->authorized_actions());
-        sc->public_actions().clear();
-        auth::from_default_privilege(idef.table_definition(), sc->public_actions());
-
-        sharksfin::StorageOptions options{};
-        options.payload(std::move(storage));
-        auto stg = context.database()->get_or_create_storage(c.simple_name());
-        if(! stg) {
-            // should not happen normally
-            set_error(
-                context,
-                error_code::target_not_found_exception,
-                string_builder{} << "Storage \"" << c.simple_name() << "\" not found" << string_builder::to_string,
-                status::err_not_found
-            );
-            return false;
-        }
-        if(auto res = stg->set_options(options);res != status::ok) {
-            // should not happen normally
-            // though this calls sharksfin, updating storage metadata should almost always succeed
-            set_error(
-                context,
-                error_code::sql_execution_exception,
-                string_builder{} << "failed to modify storage metadata. status:" << res << string_builder::to_string,
-                status::err_unknown
-            );
+        auto [public_actions, authorized_actions] = calculate_public_and_authorized_actions(tpe, *sc, grant, current_user);
+        if(! serialize_and_save(context, c.simple_name(), *sc, public_actions, authorized_actions)) {
             return false;
         }
     }
     return true;
+}
+
+bool process_grant_revoke(  //NOLINT(readability-function-cognitive-complexity)
+    bool grant,
+    request_context& context,
+    std::vector<takatori::statement::details::table_privilege_element> const& elements
+) {
+    BOOST_ASSERT(context.storage_provider());  //NOLINT
+    std::optional<std::string_view> current_user = context.req_info().request_source()
+        ? context.req_info().request_source()->session_info().username()
+        : std::nullopt;
+
+    // pre-condition check
+    // - all target tables exist
+    // - syntax elements are correct (e.g. current_user is allowed only when user name is available)
+    // - acquire locks successfully
+    // - current user has sufficient permission
+    if(! check_grant_revoke_preconditions(grant, context, elements, current_user)) {
+        return false;
+    }
+
+    // pre-condition checked, let's make changes
+    // after this point, we expect no error occurs normally; if any error occurs, that is unexpected internal error
+    return reflect_grant_revoke(grant, context, elements, current_user);
 }
 
 }

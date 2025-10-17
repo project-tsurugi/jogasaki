a40eaf214ea2972d3d70d2aea0b37d226d465c11
Yoshiaki Nishimura <nishimura@nautilus-technologies.com>
2025-10-17 15:03:38 +0900

    from to_string to to_string_view

--- src/jogasaki/udf/plugin_api.cpp
+++ src/jogasaki/udf/plugin_api.cpp
@@ -26,44 +26,47 @@
 #include "generic_record_impl.h"
 
 namespace plugin::udf {
-std::string to_string(function_kind_type kind) {
+namespace {
+std::string_view to_string_view(function_kind_type kind) {
+    using namespace std::literals;
     switch(kind) {
-        case function_kind_type::Unary: return "Unary";
-        case function_kind_type::ClientStreaming: return "ClientStreaming";
-        case function_kind_type::ServerStreaming: return "ServerStreaming";
-        case function_kind_type::BidirectionalStreaming: return "BidirectionalStreaming";
-        default: return "UnknownFunctionKind";
+        case function_kind_type::Unary: return "Unary"sv;
+        case function_kind_type::ClientStreaming: return "ClientStreaming"sv;
+        case function_kind_type::ServerStreaming: return "ServerStreaming"sv;
+        case function_kind_type::BidirectionalStreaming: return "BidirectionalStreaming"sv;
+        default: return "UnknownFunctionKind"sv;
     }
 }
 
-std::string to_string(type_kind_type kind) {
+std::string_view to_string_view(type_kind_type kind) {
+    using namespace std::literals;
     switch(kind) {
-        case type_kind_type::FLOAT8: return "FLOAT8";
-        case type_kind_type::FLOAT4: return "FLOAT4";
-        case type_kind_type::INT8: return "INT8";
-        case type_kind_type::UINT8: return "UINT8";
-        case type_kind_type::INT4: return "INT4";
-        case type_kind_type::FIXED8: return "FIXED8";
-        case type_kind_type::FIXED4: return "FIXED4";
-        case type_kind_type::BOOL: return "BOOL";
-        case type_kind_type::STRING: return "STRING";
-        case type_kind_type::GROUP: return "GROUP";
-        case type_kind_type::MESSAGE: return "MESSAGE";
-        case type_kind_type::BYTES: return "BYTES";
-        case type_kind_type::UINT4: return "UINT4";
-        case type_kind_type::ENUM: return "ENUM";
-        case type_kind_type::SINT4: return "SINT4";
-        case type_kind_type::SINT8: return "SINT8";
-        case type_kind_type::SFIXED8: return "SFIXED8";
-        case type_kind_type::SFIXED4: return "SFIXED4";
-        default: return "UnknownTypeKind";
+        case type_kind_type::FLOAT8: return "FLOAT8"sv;
+        case type_kind_type::FLOAT4: return "FLOAT4"sv;
+        case type_kind_type::INT8: return "INT8"sv;
+        case type_kind_type::UINT8: return "UINT8"sv;
+        case type_kind_type::INT4: return "INT4"sv;
+        case type_kind_type::FIXED8: return "FIXED8"sv;
+        case type_kind_type::FIXED4: return "FIXED4"sv;
+        case type_kind_type::BOOL: return "BOOL"sv;
+        case type_kind_type::STRING: return "STRING"sv;
+        case type_kind_type::GROUP: return "GROUP"sv;
+        case type_kind_type::MESSAGE: return "MESSAGE"sv;
+        case type_kind_type::BYTES: return "BYTES"sv;
+        case type_kind_type::UINT4: return "UINT4"sv;
+        case type_kind_type::ENUM: return "ENUM"sv;
+        case type_kind_type::SINT4: return "SINT4"sv;
+        case type_kind_type::SINT8: return "SINT8"sv;
+        case type_kind_type::SFIXED8: return "SFIXED8"sv;
+        case type_kind_type::SFIXED4: return "SFIXED4"sv;
+        default: return "UnknownTypeKind"sv;
     }
 }
-namespace {
+
 void add_column(const std::vector<column_descriptor*>& cols) {
     for(const auto* col: cols) {
         std::cout << "- column_name: " << col->column_name() << std::endl;
-        std::cout << "  type_kind: " << plugin::udf::to_string(col->type_kind()) << std::endl;
+        std::cout << "  type_kind: " << plugin::udf::to_string_view(col->type_kind()) << std::endl;
 
         if(auto nested = col->nested()) {
             std::cout << "  nested_record:" << std::endl;
@@ -80,7 +83,7 @@ void print_columns(const std::vector<column_descriptor*>& cols, int indent = 0) 
 
     for(const auto* col: cols) {
         std::cout << indent_str << "- column_name: " << col->column_name() << std::endl;
-        std::cout << indent_str << "  type_kind: " << plugin::udf::to_string(col->type_kind()) << std::endl;
+        std::cout << indent_str << "  type_kind: " << plugin::udf::to_string_view(col->type_kind()) << std::endl;
 
         if(auto nested = col->nested()) {
             std::cout << indent_str << "  nested_record:" << std::endl;
@@ -104,7 +107,8 @@ void print_plugin_info(const std::shared_ptr<plugin_api>& api) {
             for(const auto* fn: svc->functions()) {
                 std::cout << "          - function_name: " << fn->function_name() << std::endl;
                 std::cout << "            function_index: " << fn->function_index() << std::endl;
-                std::cout << "            function_kind: " << plugin::udf::to_string(fn->function_kind()) << std::endl;
+                std::cout << "            function_kind: " << plugin::udf::to_string_view(fn->function_kind())
+                          << std::endl;
 
                 const auto& input = fn->input_record();
                 std::cout << "            input_record:" << std::endl;
